712c03ef6c4af40eb0a05692c1a9e717509caab3
Myh <mihai.dumitru@gxgnet.ro>
2010-08-03 22:34:36 +0300

    [317] *Add m_duringRemoveFromWorld lock to prevent adding single target auras during
    or after remove from world process.

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1944,6 +1944,7 @@ class Unit : public WorldObject
 
         void OutDebugInfo() const;
         virtual bool isBeingLoaded() const { return false;}
+        bool IsDuringRemoveFromWorld() const {return m_duringRemoveFromWorld;}
 
         Pet* ToPet(){ if (isPet()) return reinterpret_cast<Pet*>(this); else return NULL; }
         Totem* ToTotem(){ if (isTotem()) return reinterpret_cast<Totem*>(this); else return NULL; }
@@ -2061,7 +2062,8 @@ class Unit : public WorldObject
         uint32 m_reducedThreatPercent;
         uint64 m_misdirectionTargetGUID;
 
-        bool m_cleanupDone;
+        bool m_cleanupDone; // lock made to not add stuff after cleanup before delete
+        bool m_duringRemoveFromWorld; // lock made to not add stuff after begining removing from world
 };
 
 namespace Trinity
