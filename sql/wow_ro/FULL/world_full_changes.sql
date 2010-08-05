ee4658897663818357c34e50d4f33551b67019b1
Myh <mihai.dumitru@gxgnet.ro>
2010-08-05 17:05:44 +0300

    [380] RS texts cleanup.

--- sql/wow_ro/FULL/world_full_changes.sql
+++ sql/wow_ro/FULL/world_full_changes.sql
@@ -2618,34 +2618,34 @@ DELETE FROM script_texts where `entry` <= -1752001 AND `entry` >= -1752006;
 DELETE FROM script_texts where `entry` <= -1752017 AND `entry` >= -1752036;
 
 INSERT INTO script_texts (`entry`, `content_default`, `npc_entry`, `content_loc3`, `sound`, `type`, `language`) VALUES
-('-1752008', '', '0', 'Help I''m caught in that tree! Help me!', '17490', '1', '0'),	 	
-('-1752009', '', '0', 'Thank you', '17491', '1', '0'),	 	
-('-1752010', '', '0', 'We believed the Sanctum is well appreciated but this type of attack we were not prepared ...', '17492', '0', '0'),	 	
-('-1752011', '', '0', 'The Black Dragonkin appeared from nowhere and attacked us to respond before we could ...', '17493', '0', '0'), 	
-('-1752012', '', '0', 'We had no chance ... While my brothers died, I could retire here and the entrance block.', '17494', '0', '0'), 	
-('-1752013', '', '0', 'They slaughtered us with cold efficiency ... But their wares goal seemed to be the eggs which are located in the Sanctum.', '17495', '0', '0'),	
-('-1752014', '', '0', 'The commander of ground troops is a cruel beast called Zarithrian. But I fear greater powers are at work ...', '17496', '0', '0'),	
-('-1752015', '', '0', 'I saw in their attack shortly their true leader ... a terrible twilight adult dragon.', '17497', '0', '0'),	
-('-1752016', '', '0', 'I know their exact plans but I know one thing: They may not be successful!', '17498', '0', '0'),
+('-1752008', 'Help! I am trapped within this tree! I require aid!', '0', '', '17490', '1', '0'),	 	
+('-1752009', 'Thank you! I could not have held out for much longer.... A terrible thing has happened here.', '0', '', '17491', '1', '0'),	 	
+('-1752010', 'We believed the Sanctum was well-fortified, but we were not prepared for the nature of this assault.', '0', '', '17492', '0', '0'),	 	
+('-1752011', 'The Black dragonkin materialized from thin air, and set upon us before we could react.', '0', '', '17493', '0', '0'), 	
+('-1752012', 'We did not stand a chance. As my brethren perished around me, I managed to retreat here and bar the entrance.', '0', '', '17494', '0', '0'), 	
+('-1752013', 'They slaughtered us with cold efficiency, but the true focus of their interest seemed to be the eggs kept here in the Sanctum.', '0', '', '17495', '0', '0'),	
+('-1752014', 'The commander of the forces on the ground here is a cruel brute named Zarithrian, but I fear there are greater powers at work.', '0', '', '17496', '0', '0'),	
+('-1752015', 'In their initial assault, I caught a glimpse of their true leader, a fearsome full-grown twilight dragon.', '0', '', '17497', '0', '0'),	
+('-1752016', 'I know not the extent of their plans, heroes, but I know this: They cannot be allowed to succeed!', '0', '', '17498', '0', '0'),
 
-( -1752001, "Ah, the entertainment has arrived.", 0, "Ahh...Die Unterhaltung ist eingetroffen!", 17520, 1, 0),
-( -1752002, "Baltharus leaves nobody alive!", 0, "Baltharus lasst niemanden am Leben!", 17521, 1, 0),
-( -1752003, "This world has enough heroes.", 0, "Diese Welt hat genug Helden!", 17522, 1, 0),
-( -1752004, "I..didn''t saw...that coming...", 0, "Das...habe ich...nicht..kommen sehen...", 17523, 1, 0),
-( -1752005, "Twice the pain and half the fun.", 0, "Doppelter Schmerz, Halb so viel Spa?!", 17524, 1, 0),
-( -1752006, "", 0, "Eure Macht schwindet, Uralte! Balt folgt ihr euren Freunden!", 17525, 1, 0),
+( -1752001, "Ah, the entertainment has arrived.", 0, "", 17520, 1, 0),
+( -1752002, "Baltharus leaves no survivors!", 0, "", 17521, 1, 0),
+( -1752003, "This world has enough heroes.", 0, "", 17522, 1, 0),
+( -1752004, "I..Didn''t saw...that coming...", 0, "", 17523, 1, 0),
+( -1752005, "Twice the pain and half the fun.", 0, "", 17524, 1, 0),
+( -1752006, "Your power wanes, ancient one.... Soon you will join your friends.", 0, "", 17525, 1, 0),
 
-( -1752017, "Alexstrasza has chosen capable allies... A pity that I must END YOU!", 0, "Alextrasza hat fahige Verbundete... Zu Schade, dass es hier mit euch ENDET!", 17512, 1, 0),
-( -1752018, "You thought you stood a chance?", 0, "Ihr dachtet ihr konntet bestehen?", 17513, 1, 0),
-( -1752019, "It''s for the best.", 0, "Es ist zu eurem Besten.", 17514, 1, 0),
-( -1752020, "HALION! I...", 0, "HALION! Ich...", 17515, 1, 0),
-( -1752021, "Turn them to ashes, minions!", 0, "Aschert sie ein, Lakeien!", 17516, 1, 0),
+( -1752017, "Alexstrasza has chosen capable allies... A pity that I must END YOU!", 0, "", 17512, 1, 0),
+( -1752018, "You thought you stood a chance?", 0, "", 17513, 1, 0),
+( -1752019, "It''s for the best.", 0, "", 17514, 1, 0),
+( -1752020, "HALION! I...", 0, "", 17515, 1, 0),
+( -1752021, "Turn them to ashes, minions!", 0, "", 17516, 1, 0),
 
-( -1752022, "You will sssuffer for this intrusion!", 0, "Fur dieses Eindringen werdet ihr leiden!", 17528, 1, 0),
-( -1752023, "As it should be...", 0, "Wie es sich gehort...", 17529, 1, 0),
-( -1752024, "Halion will be pleased", 0, "Halion wird erfreut sein...", 17530, 1, 0),
-( -1752025, "Hhrr...Grr..", 0, "HHrr...Grr..", 17531, 1, 0),
-( -1752026, "Burn in the master's flame!", 0, "Brennt in der Flamme des Meisters!", 17532, 1, 0),
+( -1752022, "You will sssuffer for this intrusion!", 0, "", 17528, 1, 0),
+( -1752023, "As it should be...", 0, "", 17529, 1, 0),
+( -1752024, "Halion will be pleased", 0, "", 17530, 1, 0),
+( -1752025, "Hhrr...Grr..", 0, "", 17531, 1, 0),
+( -1752026, "Burn in the master's flame!", 0, "", 17532, 1, 0),
 
 (-1752027, 'Insects! You''re too late. The Ruby Sanctum is lost.',NULL,NULL,17499,0,0),
 (-1752028, 'Your world teeters on the brink of annihilation. You will ALL bear witness to the coming of a new age of DESTRUCTION!',NULL,NULL,17500,0,0),
@@ -2669,7 +2669,6 @@ UPDATE `creature_template` SET `ScriptName` = 'boss_halion' WHERE `entry`= '3986
 UPDATE `creature_template` SET `ScriptName` = 'boss_twilight_halion' WHERE `entry` = '40142';
 UPDATE `creature_template` SET `ScriptName` = 'npc_onyx_flamecaller' WHERE `entry` = '39814';
 UPDATE `gameobject_template` SET `ScriptName` = 'go_firefield' WHERE `entry` = '203005';
-
 DELETE FROM `spell_bonus_data` WHERE `entry` IN (20187,54158);
 
 -- Limit Flame Tsunami buff to Lava Blazes only
