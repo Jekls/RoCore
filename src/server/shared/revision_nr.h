72bad154f4371281bbb79f5a9f1681d45c753baa
Myh <mihai.dumitru@gxgnet.ro>
2010-08-06 03:43:04 +0300

    [394] Fix conflict.

--- src/server/shared/revision_nr.h
+++ src/server/shared/revision_nr.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
-<<<<<<< HEAD:src/server/shared/revision_nr.h
- #define REVISION_NR "393"
-=======
- #define REVISION_NR "391"
->>>>>>> 34465c3fa601e2b22d227de75bd9683c6b27a0fc:src/server/shared/revision_nr.h
+ #define REVISION_NR "394"
 #endif // __REVISION_NR_H__
